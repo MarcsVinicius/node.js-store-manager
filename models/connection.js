13544e999a90038bd86e450df625a8c3ce9f8ac7
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-05-10 17:00:19 -0300

    fix: update SQL connection settings

--- models/connection.js
+++ models/connection.js
@@ -3,9 +3,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER || 'root',
-    password: 'docker',
-    database: 'StoreManager',
-    port: '3306',
+    password: process.env.MYSQL_PASSWORD || 'docker',
+    database: process.env.MYSQL_DATABASE || 'StoreManager',
   });
   
   module.exports = connection;
