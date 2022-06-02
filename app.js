f482e326ca6a65eb14c22a354c15af77d870decf
MarcsVinicius <marcos.vgalencar@gmail.com>
2022-06-02 08:07:12 -0300

    config to github

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const router = require('./routes/index');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
@@ -13,7 +12,4 @@ app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
